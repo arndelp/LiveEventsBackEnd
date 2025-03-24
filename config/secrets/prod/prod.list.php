7809501f6ce5e519d10be0d01eb369041fc85d7e
arndelp <arndelp@yahoo.fr>
2025-03-24 09:30:04 +0100

    V1

---
+++ config/secrets/prod/prod.list.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'APP_SECRET' => null,
+    'CORS_ALLOW_ORIGIN' => null,
+    'DATABASE_URL' => null,
+    'GOOGLE_RECAPTCHA_SECRET_KEY' => null,
+    'MAILER_DSN' => null,
+    'MONGODB_DB' => null,
+    'MONGODB_URL' => null,
+];
