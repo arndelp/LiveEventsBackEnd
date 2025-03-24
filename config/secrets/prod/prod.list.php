3740e77ad4270a836d76a4759fcae8f4c2923eea
arndelp <arndelp@yahoo.fr>
2025-03-24 14:00:16 +0100

    V1

--- config/secrets/prod/prod.list.php
+++ config/secrets/prod/prod.list.php
@@ -5,7 +5,6 @@ return [
     'DATABASE_PWD' => null,
     'DATABASE_USER' => null,
     'GOOGLE_RECAPTCHA_SECRET_KEY' => null,
-    'MAILER_DSN' => null,
     'MONGODB_DB' => null,
     'MONGODB_PWD' => null,
     'MONGODB_USER' => null,
