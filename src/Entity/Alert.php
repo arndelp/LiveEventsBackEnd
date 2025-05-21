12ad44537a6235d5d39db8355188bd30d56084fd
arndelp <arndelp@yahoo.fr>
2025-05-21 10:21:02 +0200

    V2 assert

--- src/Entity/Alert.php
+++ src/Entity/Alert.php
@@ -10,7 +10,6 @@ use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: AlertRepository::class)]
 #[ApiResource]
-#[ApiResource(normalizationContext: ['groups' => ['alert']])]
 class Alert
 {
     #[ORM\Id]
