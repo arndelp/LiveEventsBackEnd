b4465d2d525cf0c06c8ec71270b611227525ed8b
arndelp <arndelp@yahoo.fr>
2025-09-30 00:15:05 +0200

    V3

--- src/Customers/Domain/Entity/Customer.php
+++ src/Customers/Domain/Entity/Customer.php
@@ -83,7 +83,7 @@ class Customer implements  UserInterface, PasswordAuthenticatedUserInterface
     #[Groups(['customers'])]
     private ?string $phone = null;
 
-    #[ORM\Column(name: 'isVerified',type: 'boolean')]
+    #[ORM\Column(name: 'is_Verified',type: 'boolean')]
     #[Groups(['customers'])]
     private bool $isVerified = false;
 
