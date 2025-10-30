068e0168cb030aec6cb4c5f52c9e1b27ff46cad5
arndelp <arndelp@yahoo.fr>
2025-10-30 13:29:48 +0100

    captcha

--- config/secrets/prod/prod.list.php
+++ config/secrets/prod/prod.list.php
@@ -5,6 +5,8 @@ return [
     'DATABASE_PWD' => null,
     'DATABASE_USER' => null,
     'GOOGLE_RECAPTCHA_SECRET_KEY' => null,
+    'JWT_PASSPHRASE' => null,
+    'MAILER_PASSWORD' => null,
     'MONGODB_DB' => null,
     'MONGODB_PWD' => null,
     'MONGODB_USER' => null,
